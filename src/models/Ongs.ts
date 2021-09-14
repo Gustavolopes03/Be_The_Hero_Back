3ef31ab928abcc10faed4d674e87977fd5187918
Gustavo616 <gustavolavrasmg@hotmail.com>
2021-09-14 17:26:50 -0300

    1.1.0

---
+++ src/models/Ongs.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, CreateDateColumn,PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity ('ongs')
+class Ongs{
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    whatsapp: string;
+
+    @Column()
+    city: string;
+
+    @Column()
+    uf: string;
+
+    @CreateDateColumn()
+    creat_at: Date
+
+    @CreateDateColumn()
+    updated_at: Date
+
+}
+
+export default Ongs
